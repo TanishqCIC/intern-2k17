c2577a4f485db675cd136cdb801f4aa0776ca302
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-07-22 19:14:11 +0530

    Update algo_cooccurence.py

--- algo_cooccurence.py
+++ algo_cooccurence.py
@@ -2,7 +2,26 @@
    Cluster Innovation Centre, University of Delhi
 """
 
+concepts = []
+concepts.append("process management in thread")
+concepts.append("management")
 
+
+
+for alpha_index in concepts:
+	leng = len(alpha_index)
+	flag = 0
+	for beta_index in alpha_index:
+		if beta_index == " ":
+			flag = flag+1
+	#call function
+	"""
+	if flag == 1:
+		
+	"""
+	print(flag)
+	
+"""
 #Enter the name of the .txt file  down here
 filename = 'galvin_sections/3.1.1.txt'
 #Just an array to fetch each word of .txt file as an element to this array
@@ -15,7 +34,8 @@ with open(filename,'r') as f:
 
 #Printing entire .txt file			
 print(text)
-
+"""
+"""
 #introducing the array of concepts here
 concepts = []
 concepts[0] = 
@@ -23,3 +43,4 @@ concepts[1] =
 
 for word in text:
 	if word == concepts[0]
+"""
