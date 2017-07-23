05f435d3c7f809ebeccb53f6881967d66addd7cd
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-07-24 00:42:49 +0530

    Update match_cooccurence.py

--- match_cooccurence.py
+++ match_cooccurence.py
@@ -1,21 +1,11 @@
-
+import filter_text as ft
 #flag_alpha over here is flag_alpha+1 in algo_cooccurence.py
 #same follows for flag_beta
 
 def match(c1, c2, flag_alpha, flag_beta):
 
-	#Enter the name of the .txt file  down here
 	filename = 'galvin_sections/3.1.txt'
-	#Just an array to fetch each word of .txt file as an element to this array
-	text = []
-
-	with open(filename,'r') as f:
-	    for line in f:
-	        for word in line.split():
-				text.append(word)
-
-	#Printing entire .txt file			
-	#print(text)
+	text = ft.text(filename)
 
 	count_c1 = 0
 	count_c2 = 0
@@ -73,3 +63,59 @@ def match(c1, c2, flag_alpha, flag_beta):
 			alpha_count = alpha_count+1
 	print('alpha', alpha_count)
 	
+	beta_count = 0
+	for cursor_text in range(0, len(text)):
+		flag_match = 0
+		for cursor_beta in range(0, len(con_c2)):
+			#print('index_text', cursor_text+cursor_alpha, 'ind_alpha', cursor_alpha)
+			if cursor_beta+cursor_text<len(text):
+				if text[cursor_text+cursor_beta] == con_c2[cursor_beta]:
+					if flag_match != 2:
+						flag_match = 1
+				else:
+					flag_match = 2
+		if flag_match == 1:
+			beta_count = beta_count+1
+	print('beta', beta_count)
+
+
+
+	"""
+	#finding c1 in text
+	c1_count = 0
+	break_flag = 0
+	for cursor_alpha in range(0, len(text)):
+		print('break', break_flag)
+		for index_alpha in range(0, len(con_c1)):
+			if text[cursor_alpha+index_alpha] == con_c1[index_alpha]:
+				flag_match = 1
+			else:
+				flag_match = 0
+				break_flag = 1
+				break
+
+		if flag_match == 1:
+			c1_count = c1_count + 1
+	if c1_count != 0:
+		print('c1 found', c1_count)
+		c1_count=0
+
+	#finding c2 in text
+	break_flag = 0
+	c2_count = 0
+	for cursor_beta in range(0, len(text)):
+		for index_beta in range(0, len(con_c2)):
+			if text[cursor_beta+index_beta] == con_c1[index_beta]:
+				print('text', text[cursor_beta+index_beta], 'concept', con_c1[index_beta])
+				flag_match = 1
+			else:
+				flag_match = 0
+				print('flag down')
+				break
+
+		if flag_match == 1:
+			c2_count = c2_count + 1
+	if c2_count != 0:
+		print('c2 found', c2_count)
+		c2_count = 0
+"""
