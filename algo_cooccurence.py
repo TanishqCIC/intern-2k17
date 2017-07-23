d8dda37a6992ecb892e1a97e982cc8436ea7920b
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-07-23 16:08:21 +0530

    Update algo_cooccurence.py

--- algo_cooccurence.py
+++ algo_cooccurence.py
@@ -5,12 +5,55 @@
 concepts = []
 concepts.append("process management in thread")
 concepts.append("management")
+concepts.append("process management")
+concepts.append("processes and thread")
 
 #threshold is often defined as e in the paper
 threshold = 2
 
+for alpha in range(0, len(concepts)-1):
+	for beta in range(alpha+1, len(concepts)):
+		flag_alpha = 0
+		flag_beta = 0
+		c1 = concepts[alpha]
+		c2 = concepts[beta]
+		for space_alpha in range(0, len(concepts[alpha])):
+			if c1[space_alpha] == " ":
+				flag_alpha = flag_alpha+1
+		for space_beta in range(0, len(concepts[beta])):
+			if c2[space_beta] == " ":
+				flag_beta = flag_beta+1
+		print('pairs', alpha, beta)
+		print('flags')
+		print(flag_alpha+1, flag_beta+1)
+		"""call function
+		match_cooccurence(c1, c2, flag_alpha+1, flag_beta+1)
+		"""
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 for alpha_index in concepts[:-1]:
 	leng_c1 = len(alpha_index)
 	leng_c2 = len(alpha_index+1)
@@ -23,12 +66,11 @@ for alpha_index in concepts[:-1]:
 		if beta_index == " ":
 			flag_c2 = flag_c2+1
 	#call function
-	"""
 	match(flag)
 
 	"""
-	print(flag_c1)
-	print(flag_c2)
+	#print(flag_c1)
+	#print(flag_c2)
 	
 """
 #Enter the name of the .txt file  down here
