55f03f692be93a848c3b6c331464eed0781b9ce5
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-07-24 15:11:55 +0530

    Update algo_cooccurence.py

--- algo_cooccurence.py
+++ algo_cooccurence.py
@@ -2,16 +2,32 @@
    Cluster Innovation Centre, University of Delhi
 """
 
+import match_cooccurrence as mc
+
 concepts = []
-concepts.append("process management in thread")
-concepts.append("management")
-concepts.append("process management")
-concepts.append("processes and thread")
+#concepts from section 3.1
+concepts.append("process control block")
+concepts.append("task control block")
+concepts.append("multithreaded processes")
+concepts.append("CPU-scheduling")
+concepts.append("Memory-management")
+concepts.append("task")
+#concepts from 3.3
+concepts.append("system call")
+concepts.append("process execution")
 
+
+filenames = []
+filenames.append('galvin_sections/3.1.txt')
+filenames.append('galvin_sections/3.2.txt')
+filenames.append('galvin_sections/3.3.txt')
+
+print('Sections Involved', filenames)
 #threshold is often defined as e in the paper
-threshold = 2
+threshold = 0
+array = []
 
-for alpha in range(0, len(concepts)-1):
+for alpha in range(0, len(concepts)):
 	for beta in range(alpha+1, len(concepts)):
 		flag_alpha = 0
 		flag_beta = 0
@@ -23,12 +39,41 @@ for alpha in range(0, len(concepts)-1):
 		for space_beta in range(0, len(concepts[beta])):
 			if c2[space_beta] == " ":
 				flag_beta = flag_beta+1
-		print('pairs', alpha, beta)
-		print('flags')
-		print(flag_alpha+1, flag_beta+1)
-		"""call function
-		match_cooccurence(c1, c2, flag_alpha+1, flag_beta+1)
-		"""
+		#print('pairs', alpha, beta)
+		#print('flags', flag_alpha+1, flag_beta+1)
+		#call function
+		
+
+		alpha_count, beta_count = mc.match(c1, c2, flag_alpha+1, flag_beta+1, filenames)
+		raw = (c1, alpha_count, c2, beta_count)
+		array.append(raw)
+		#print('-----')
+#print(array)
+print('-------')
+print('total links possible', len(array))
+
+link = []
+for elements in array:
+	turn = 0
+	concept_1 = elements[0]		
+	concept_2 = elements[2]
+	for flags in range(0, len(elements[1])):
+		if elements[1][flags] > 0:
+			if elements[3][flags] > 0:
+				turn = turn + 1
+	#print(turn)
+	if turn > threshold:
+		link_concepts = (concept_1, concept_2)
+		link.append(link_concepts)
+print('-------')
+print('LINKED CONCEPTS', len(link))
+print('-------')
+for concepts in link:
+	to_print = (concepts[0] + ' & ' + concepts[1])
+	print(to_print)
+#print(link)
+print('-------')
+print('www.github.com/TanishqCIC')
 
 
 
@@ -52,46 +97,3 @@ for alpha in range(0, len(concepts)-1):
 
 
 
-
-"""
-for alpha_index in concepts[:-1]:
-	leng_c1 = len(alpha_index)
-	leng_c2 = len(alpha_index+1)
-	flag_c1 = 0
-	flag_c2 = 0
-	for beta_index in alpha_index:
-		if beta_index == " ":
-			flag_c1 = flag_c1+1
-	for gamma_index in alpha_index:
-		if beta_index == " ":
-			flag_c2 = flag_c2+1
-	#call function
-	match(flag)
-
-	"""
-	#print(flag_c1)
-	#print(flag_c2)
-	
-"""
-#Enter the name of the .txt file  down here
-filename = 'galvin_sections/3.1.1.txt'
-#Just an array to fetch each word of .txt file as an element to this array
-text = []
-
-with open(filename,'r') as f:
-    for line in f:
-        for word in line.split():
-			text.append(word)
-
-#Printing entire .txt file			
-print(text)
-"""
-"""
-#introducing the array of concepts here
-concepts = []
-concepts[0] = 
-concepts[1] =
-
-for word in text:
-	if word == concepts[0]
-"""
